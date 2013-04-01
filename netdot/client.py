ad2fa7e6a96db8a3834180bffc72c187c49c98fd
Francisco Gray <frgray@uoregon.edu>
2013-04-01 15:12:55 -0700

    Adding significant updates to docstring

--- netdot/client.py
+++ netdot/client.py
@@ -38,21 +38,16 @@ __version__ = '0.03' ## Not always updated
 
 class client(object):
 	def __init__(self, username, password, server, debug = 0):
-		"""__init__():
-				Usage:
-					uname = 'my_user'
-					pword = 'my_pass'
-					server = "https://netdot.localdomain/netdot"
-					import netdot
-					dot = netdot.client(uname,pword,server)
-			
-				Description: 
-					Class constructor, instantiates a number of 
-				variables for use in the class.  Mainly the required
-				NetDot HTTP headers and login form parameters.
-			
-				Returns: 
-					NetDot.client object.
+		"""
+		Class constructor, instantiates a number of 
+		variables for use in the class.  Mainly the required
+		NetDot HTTP headers and login form parameters.
+		
+		Usage:
+			import netdot
+			dot = netdot.client(username,password,"https://netdot.localdomain/netdot",[debug])
+						
+		Returns: NetDot.client object.
 		"""
 		if debug == 1:
 			self.debug = True
@@ -79,11 +74,16 @@ class client(object):
 			raise AttributeError('Username, Password and Server are REQUIRED')
 		
 	def _login(self,params):
-		"""_login():
-				Description: 
-					Internal Function. Logs into the NetDot API with provided credentials, 
-				stores the Apache generated cookies into the self object to be 
-				reused.  
+		"""
+		Internal Function. Logs into the NetDot API with provided credentials, 
+		stores the Apache generated cookies into the self object to be 
+		reused.  
+		
+		Arguments:
+			dict -- 'destination':'index.html', 
+							'credential_0':username, 
+							'credential_1':password, 
+							'permanent_session':1
 		"""
 		response = requests.post(self.login_url, data=params, headers=self.headers)
 		if response.status_code == 200:
@@ -92,18 +92,20 @@ class client(object):
 			raise AttributeError('Invalid Credentials')
 			
 	def get(self, url):
-		"""get():
-				Usage:
-					response = netdot.client.get("/url")
+		"""
+		This function provides a simple interface
+		into the "GET" function by handling the authentication
+		cookies as well as the required headers and base_url for 
+		each request.	
+		
+		Arguments:
+			url -- Url to append to the base url
+		
+		Usage: 
+			response = netdot.client.get("/url")
 			
-				Description: 
-					This function provides a simple interface
-				into the "GET" function by handling the authentication
-				cookies as well as the required headers and base_url for 
-				each request.  
-			
-				Returns: 
-					Result as a multi-level dictionary on sucess. 
+		Returns: 
+			Result as a multi-level dictionary on sucsess. 
 		"""
 		response = requests.get(self.base_url + url, cookies=self.auth_cookies, headers=self.headers)
 		if hasattr(self, 'debug'):
@@ -122,17 +124,20 @@ class client(object):
 			raise Exception('Non-200 Return Code: %s, Content: %s' % (response.status_code, response.content))
 		
 	def post(self, url, data):
-		"""post():
-				Usage:
-					response = netdot.client.post("/url", {form-data})
-			
-				Description: 
-					This function provides a simple interface
-				into the "POST" function by handling the authentication
-				cookies as well as the required headers and base_url for 
-				each request.  
-			
-				Returns: 
+		"""
+		This function provides a simple interface
+		into the "POST" function by handling the authentication
+		cookies as well as the required headers and base_url for 
+		each request.  		
+		
+		Arguments:
+			url -- Url to append to the base url
+			data -- dict of key/value pairs that the form requires
+		
+		Usage:
+			response = netdot.client.post("/url", {form-data})
+		
+		Returns: 
 					Result as a multi-level dictionary on success
 		"""
 		response = requests.post(self.base_url + url, cookies=self.auth_cookies, data=data, headers=self.headers)
@@ -152,18 +157,20 @@ class client(object):
 			raise Exception('Non-200 Return Code: %s, Content: %s' % (response.status_code, response.content))
 		
 	def delete(self, url):
-		"""delete():
-				Usage: 
-					response = netdot.client.delete("/url")
+		"""
+		This function provides a simple interface
+		into the "HTTP/1.0 DELETE" function by handling the authentication
+		cookies as well as the required headers and base_url for 
+		each request.  		
 
-				Description: 
-					This function provides a simple interface
-				into the "DELETE" function by handling the authentication
-				cookies as well as the required headers and base_url for 
-				each request.  
+		Arguements:
+			url -- Url to append to the base url
+		
+		Usage: 
+			response = netdot.client.delete("/url")
 
-				Returns: 
-					Result as a multi-level dictionary
+		Returns: 
+			Result as an empty multi-level dictionary
 		"""
 		response = requests.delete(self.base_url + url, cookies=self.auth_cookies, headers=self.headers)
 		if hasattr(client, 'debug'):
@@ -182,97 +189,112 @@ class client(object):
 			raise Exception('Non-200 Return Code: %s, Content: %s' % (response.status_code, response.content))
 				
 	def getHostByIPID(self, id):
-		"""getHostByIPID():
-				Usage:
-					response = netdot.client.getHostByIPID("1111")
-					
-				Description: 
-					This function returns a NetDot-XML object 
-				for the requested IP ID.
-				
-				Returns:
-					Multi-level dictionary on success.
+		"""
+		This function returns a NetDot-XML object 
+		for the requested IP ID.
+		
+		Arguments: 
+			id -- NetDot IP ID
+		
+		Usage:
+			response = netdot.client.getHostByIPID("1111")
+		
+		Returns:
+			Multi-level dictionary on success.
 		"""
 		return self._parse(id, self.get("/host?ipid=" + id))
 
 	def getHostByRRID(self, id):
-		"""getHostByRRID():
-				Description: 
-					This function returns a multi-level dictionary object 
-				for the requested RR ID.
-				
-				Returns:
-					Multi-level dictionary on success.
+		"""
+		This function returns a NetDot-XML object 
+		for the requested RR ID.
+		
+		Arguments: 
+			id -- NetDot RR ID
+		
+		Usage:
+			response = netdot.client.getHostByRRID("1111")
+		
+		Returns:
+			Multi-level dictionary on success.
 		"""
 		return self._parse(id, self.get("/host?rrid=" + id))
 
 	def getHostByName(self, name):
-		"""getHostByName():
-				Usage: 
-					response = netdot.client.getHostByName("host")
-					
-				Description: 
-					This function returns a NetDot-XML object 
-				for the requested shortname.
-				
-				Returns:
-					Multi-level dictionary on success.
+		"""
+		This function returns a NetDot-XML object 
+		for the requested shortname
+		
+		Arguments: 
+			name -- DNS shortname
+		
+		Usage:
+			response = netdot.client.getHostByName("foo")
+		
+		Returns:
+			Multi-level dictionary on success.
 		"""
 		return self._parse(name, self.get("/host?name=" + name))
 		
 	def getIPBlock(self, ipblock):
-		"""getIPBlock():
-				Usage: 
-					response = netdot.client.getIPBlock('192.168.1.0/24')
-				
-				Description: 
-					This function returns all of the host 
-				records from the provided ip block.
-				
-				Returns:
-					Array of NetDot-XML objects on success
+		"""
+		This function returns all of the host 
+		records from the provided ip block.
+		
+		Arguments: 
+			ipblock - IpBlock in CIDR notation 
+		
+		Usage: 
+			response = netdot.client.getIPBlock('192.168.1.0/24')
+			
+		Returns:
+			Array of NetDot-XML objects on success
 		"""		
 		return self.get("/host?subnet=" + ipblock)
 				
 	def getHostByAddress(self, address):
-		"""getHostByAddress():
-				Usage:
-					response = netdot.client.getHostByIPID("192.168.0.1")
+		"""
+		This function returns a NetDot-XML object 
+		for the requested IP Address.		
+		
+		Arguments:
+			address -- IP Address in "dotted-quad" syntax
 
-				Description: 
-					This function returns a NetDot-XML object 
-				for the requested IP Address.
-	
-				Returns:
-					Multi-level dictionary on success.
+		Usage:
+			response = netdot.client.getHostByIPID("192.168.0.1")
+
+		Returns:
+			Multi-level dictionary on success.
 		"""
 		return self._parse(address, self.get("/host?address=" + address))
 
 	def getPersonByUsername(self, user):
-		"""getPersonByUsername():
-				Usage:
-					response = netdot.client.getPersonByUsername("user")
+		"""
+		Returns a single-level dict of the requested Username
 
-				Description:
-					This function returns a NetDot-XML object
-				for the requested Username
-				
-				Returns:
-					Multi-level dictionary on success.
+		Arguments:
+			user -- Desired username
+
+		Usage:
+			response = netdot.client.getPersonByUsername("user")
+			
+		Returns:
+			Multi-level dictionary on success.
 		"""
 		return self._parse(user, self.get("/person?username=" + user))
 
 	def getPersonById(self, id):
-		"""getPersonByid():
-				Usage:
-					response = netdot.client.getPersonById("user_id")
+		"""
+		Returns a single-level dict of the requested user id
 
-				Description:
-					This function returns a NetDot-XML object
-				for the requested Username
-				
-				Returns:
-					Multi-level dictionary on success.
+		Arguments:
+			id -- Desired User ID
+
+		Usage:
+			response = netdot.client.getPersonById("id")
+			
+		Returns:
+			Multi-level dictionary on success.
 		"""
 		xml = self.get("/person?id=" + id)
 		xml_root = ET.fromstring(xml)
@@ -283,30 +305,33 @@ class client(object):
 		return person
 
 	def getObjectByID(self, object, id):
-		"""getObjectByID():
-			Usage:
-				response = netdot.client.getObjectByID("object", "id")
-
-			Description:
-				This function returns a NetDot-XML object 
-			for the request object and id
-
-			Return:
+		"""
+		Returns a single-level dict of the requested object and id
+		
+		Arguments:
+			object -- 'device' or 'host' etc...
+			id  --  Object ID
+		
+		Usage:
+			response = netdot.client.getObjectByID("object", "id")
+		
+		Returns:
 				Multi-level dictionary on success
 		"""
 		return self.get("/" + object + "?id=" + id)
 
 	def getContactByPersonID(self, id):
-		"""getContactByPersonID():
-				Usage:
-					response = netdot.client.getContactByPersonID("id")
-
-				Description:
-					This function returns a NetDot-XML object
-				for the requested Contact
-
-				Returns:
-					Single-level dictionary on success.
+		"""
+		Returns a single-level dict of the requested Person
+		
+		Arguments:
+			id  --  person id
+		
+		Usage:
+			response = netdot.client.getContactByPersonID('id')
+		
+		Returns:
+				single-level dictionary on success
 		"""
 		xml = self.get("/contact?person=" + id)
 		xml_root = ET.fromstring(xml)
@@ -317,55 +342,70 @@ class client(object):
 		return person
 
 	def getContactByUsername(self, user):
-		"""getContactByUsername():
-				Usage:
-					response = netdot.client.getContactByUsername("foo")
-					
-				Description:
-					This function returns a NetDot-XML object
-				for the requested Contact
-				
-				Returns:
-					Single-level dictionary on success.
+		"""
+		Returns a single-level dict of the requested Username
+		
+		Arguments:
+			user  --  NetDot Username
+		
+		Usage:
+			response = netdot.client.getContactByUsername("mary")
+		
+		Returns:
+				Multi-level dictionary on success
 		"""
 		person = self.getPersonByUsername(user)
 		return self.getContactByPersonID(person['id'])		
 
 	def getGrouprightByConlistID(self, id):
-		"""getGrouprightByConlistID():
-				Usage:
-					response = netdot.client.getGrouprightByConlistID("id")
-
-				Description:
-						This function returns a NetDot-XML object
-					for the requested Contact
-
-				Returns:
-					Multi-level dictionary on success.
+		"""
+		Returns a single-level dict of the requested group's 
+		access rights
+		
+		Arguments:
+			id  --  NetDot Contact List ID
+		
+		Usage:
+			response = netdot.client.getGrouprightsByConlistID("id")
+		
+		Returns:
+				Multi-level dictionary on success
 		"""
 		return self.get("/groupright?contactlist=" + id)
 	
 	def addCnameToARecord(self, name, cname):
-		"""addCnameToARecord():
+		"""
+		This fucntion will add a CNAME to an 
+		existing resource record or "A" record
+		
+		Arguments:
+			name -- A record
+			cname -- Desired CNAME
+			
+		Usage: 
+			response = dot.addCnameToARecord('foo.example.com', 'bar.example.com')
 		"""
 		data = { 'cname': cname }
 		host = self.getHostByName(name)
 		for key in host[name]['RR'].iterkeys():
 			for attr, attr_val in host[name]['RR'][key].iteritems():
 				if attr == 'name' and attr_val == name:
-#					return self.post("/host?rrid=" + host[name]['RR'][key]['id'], data)
+					return self.post("/host?rrid=" + host[name]['RR'][key]['id'], data)
 		
 	def renameHost(self, old, new):
-		"""renameHost():
-				Usage: 
-					netdot.client.renameHost('old-name','new-name')
-				
-				Description: 
-						This function will rename a host record.  Previously, 
-					the user had to query know the RRID of the record, then 
-					post the updated name to the RRID record.  This function
-					automates the RRID search and constructs the post request 
-					for you.
+		"""
+		This function will rename a host record.  Previously, 
+		the user had to query know the RRID of the record, then 
+		post the updated name to the RRID record.  This function
+		automates the RRID search and constructs the post request 
+		for you.
+
+		Arguments:
+			old -- Old DNS shortname
+			new -- New DNS shortname
+
+		Usage: 
+			netdot.client.renameHost('old-name','new-name')
 		"""
 		host = self.getHostByName(old)
 		rrid = host['RR']['id']
@@ -374,30 +414,40 @@ class client(object):
 		return self.post("/host?rrid=" + rrid, data)
 
 	def createHost(self, data):
-		"""createHost():
-				Usage: 
-					response = netdot.client.createHost({'name':'my-server',
+		"""
+		This function takes a dict and creates a new 
+		record in the subnet '192.168.1.0/24' with an ethernet 
+		address of 'XX:XX:XX:XX:XX:XX' and a comment of 'My Server'. 
+		
+		Arguments:
+			data -- dict with at least the following key:value pairs:
+							name:'servername'
+							subnet: 'CIDR notation'
+
+		Usage: 
+			response = netdot.client.createHost({'name':'my-server',
 					'subnet':'192.168.1.0/24',
 					'ethernet':'XX:XX:XX:XX:XX:XX',
 					'info':'My Server'})
-				Description: 
-					This function takes a dict and creates a new 
-				record in the subnet '192.168.1.0/24' with an ethernet 
-				address of 'XX:XX:XX:XX:XX:XX' and a comment of 'My Server'.  
 
-				Returns: 
-					Created record as a multi-level dictionary.
+		Returns: 
+			Created record as a multi-level dictionary.
 		"""
 		return self.post("/host", data)	
 
 	def deleteHostByRRID(self, rrid):
-		"""deleteHostByRRID():
-				Usage:
-					response = netdot.client.deleteHostByRRD("1111")
-					 
-				Description: 
-					This function deletes a hostname record
-				for the requested RR ID. This also frees the IP.
+		"""
+		This function deletes a hostname record
+		for the requested RR ID. This also frees the IP.
+		
+		Arguments:
+			rrid -- NetDot Resource Record ID
+		
+		Usage:
+			response = netdot.client.deleteHostByRRD("1111")
+		
+		Returns: 
+			
 		"""
 		return self.delete("/host?rrid=" + rrid)
 		
